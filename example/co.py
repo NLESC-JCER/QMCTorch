669992765c2be58d20d5f474e427b2bc68e1f910
NicoRenaud <nicolas.gm.renaud@gmail.com>
2019-12-11 22:02:04 +0100

    removed jastrow from wf

--- example/co.py
+++ example/co.py
@@ -20,16 +20,16 @@ mol = Molecule(atom='C 0 0 0; O 0 0 2.173',
 
 
 # define the wave function
-wf = Orbital(mol, kinetic='auto',
+wf = Orbital(mol, kinetic='jacobi',
              configs='ground_state', use_projector=True)
 
 # sampler
-sampler = Metropolis(nwalkers=100, nstep=1000, step_size=0.1,
+sampler = Metropolis(nwalkers=200, nstep=1000, step_size=0.1,
                      nelec=wf.nelec, ndim=wf.ndim,
                      init=mol.domain('normal'))
 
 # optimizer
-opt = Adam(wf.parameters(), lr=0.1)
+opt = Adam(wf.parameters(), lr=0.005)
 
 # scheduler
 scheduler = optim.lr_scheduler.StepLR(opt, step_size=20, gamma=0.75)
@@ -38,12 +38,21 @@ scheduler = optim.lr_scheduler.StepLR(opt, step_size=20, gamma=0.75)
 solver = SolverOrbital(wf=wf, sampler=sampler,
                        optimizer=opt, scheduler=scheduler)
 
-# solver.configure(task='wf_opt')
-pos, e, v = solver.single_point(ntherm=500, ndecor=100)
+# # single point
+# pos, e, v = solver.single_point(ntherm=500, ndecor=100)
 
+# #  sampling traj
 # pos = solver.sample(ntherm=0, ndecor=10)
 # obs = solver.sampling_traj(pos)
 # plot_observable(obs, e0=-111., ax=None)
+
+
+# optimize the wave function
+solver.configure(task='wf_opt', freeze=['bas_exp'])
+solver.observable(['local_energy'])
+solver.run(10, loss='energy')
+plot_observable(solver.obs_dict, e0=-113.)
+
 
 # # optimize the geometry
 # solver.configure(task='geo_opt')
