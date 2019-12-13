12e53d1cd064e5ce66b8fb1d3fcaa48697eef4aa
NicoRenaud <nicolas.gm.renaud@gmail.com>
2019-12-13 16:33:10 +0100

    second der of ee and elements

--- deepqmc/wavefunction/jastrow.py
+++ deepqmc/wavefunction/jastrow.py
@@ -10,6 +10,20 @@ class ElectronDistance(nn.Module):
         self.ndim = ndim
 
     def forward(self, input, derivative=0):
+        """compute the pairwise distance between two sets of electrons
+        Or the derivative of these elements
+
+        Args:
+            input ([type]): [description]
+            derivative (int, optional): degre of the derivative.
+                                        Defaults to 0.
+
+        Returns:
+            torch.tensor: distance (or derivative) matrix
+                          Nbatch x Nelec x Nelec if derivative = 0
+                          Nbatch x Ndim x  Nelec x Nelec if derivative = 1,2
+
+        """
         '''compute the pairwise distance between two sets of electrons.
         Args:
             input1 (Nbatch,Nelec1*Ndim) : position of the electrons
@@ -28,12 +42,19 @@ class ElectronDistance(nn.Module):
             return dist
 
         elif derivative == 1:
-            invr = (1./dist).unsqueeze_(1)
 
+            invr = (1./dist).unsqueeze_(1)
             diff_axis = input.transpose(1, 2).unsqueeze_(3)
             diff_axis = diff_axis - diff_axis.transpose(2, 3)
-
             return diff_axis * invr
+
+        elif derivative == 2:
+
+            invr3 = (1./dist**3).unsqueeze(1)
+            diff_axis = input.transpose(1, 2).unsqueeze_(3)
+            diff_axis = (diff_axis - diff_axis.transpose(2, 3))**2
+            diff_axis = diff_axis[:, [[1, 2], [2, 0], [0, 1]], ...].sum(2)
+            return diff_axis * invr3
 
 
 class TwoBodyJastrowFactor(nn.Module):
@@ -82,20 +103,6 @@ class TwoBodyJastrowFactor(nn.Module):
         elif derivative == 1:
             return self._jastrow_derivative(r, jast)
 
-    def _get_jastrow_elements(self, r):
-        return torch.exp(self.static_weight * r /
-                         (1.0 + self.weight * r))
-
-    def _unique_pair_prod(self, mat, not_el=None):
-
-        mat_cpy = mat.clone()
-        if not_el is not None:
-            i, j = not_el
-            mat_cpy[..., i, j] = 1
-
-        return mat_cpy[:, torch.tril(
-            torch.ones(self.nelec, self.nelec)) == 0].prod(1).view(-1, 1)
-
     def _jastrow_derivative(self, r, jast):
 
         nbatch = r.shape[0]
@@ -115,40 +122,94 @@ class TwoBodyJastrowFactor(nn.Module):
 
         return grad_jast
 
+    def _get_jastrow_elements(self, r):
+        """Get the elements of the jastrow matrix :
+        .. math::
+            out_{i,j} = \exp{ \frac{b r_{i,j}}{1+b'r_{i,j}} }
+
+        Args:
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+
+        Returns:
+            torch.tensor: matrix fof the jastrow elements
+                          Nbatch x Nelec x Nelec
+        """
+        return torch.exp(self.static_weight * r /
+                         (1.0 + self.weight * r))
+
     def _get_der_jastrow_elements(self, r, dr):
+        """Get the elements of the derivative of the jastrow matrix
+        .. math::
+
+            out_{k,i,j} = A + B
+            A_{kij} = b \frac{dr_{ij}}{dk_i} / (1+b'r_{ij})
+            B_{kij} = - b b' r_{ij} \frac{dr_{ij}}{dk_i} / (1+b'r_{ij})^ 2
+
+        Args:
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+            dr (torch.tensor): matrix of the derivative of the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+
+        Returns:
+            torch.tensor: matrix fof the derivative of the jastrow elements
+                          Nbatch x Ndim x Nelec x Nelec
+        """
 
         r.unsqueeze_(1)
-        a = self.static_weight * dr / (1.0 + self.weight * r)
-        b = self.static_weight * self.weight * \
-            r / (1.0 + self.weight * r)**2 * dr
+        denom = 1. / (1.0 + self.weight * r)
+
+        a = self.static_weight * dr * denom
+        b = - self.static_weight * self.weight * r * dr * denom**2
         r.squeeze_()
-        return (a-b)
+        return (a+b)
 
+    def _get_second_der_jastrow_elements(self, r, dr, d2r):
+        """Get the elements of the pure 2nd derivative of the jastrow matrix
 
-class JastrowFunction(torch.autograd.Function):
-
-    @staticmethod
-    def forward(ctx, input, weight, static_weight):
-        '''Compute the Jastrow factor.
         Args:
-            input : Nbatch x Nelec x Nelec (elec distance)
-            weight : Nelec, Nelec
-            static weight : Float
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+            dr (torch.tensor): matrix of the derivative of the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+            d2r (torch.tensor): matrix of the 2nd derivative of the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+
         Returns:
-            jastrow : Nbatch x 1
-        '''
+            torch.tensor: matrix fof the pure 2nd derivative of the jastrow elements
+                          Nbatch x Ndim x Nelec x Nelec
+        """
 
-        # save the tensors
-        ctx.save_for_backward(input, weight, static_weight)
+        r.unsqueeze_(1)
+        denom = 1. / (1.0 + self.weight * r)
 
-        # all jastrow for all electron pairs
-        factors = torch.exp(static_weight * input / (1.0 + weight * input))
+        a = self.static_weight * d2r * denom
+        b = -2 * self.static_weight * self.weight * dr * denom**2
+        c = -2 * self.static_weight * self.weight**2 * r * dr * denom**3
+        r.squeeze_()
+        return a+b+c
 
-        # product of the off diag terms
-        nr, nc = input.shape[1], input.shape[2]
-        factors = factors[:, torch.tril(torch.ones(nr, nc)) == 0].prod(1)
+    def _unique_pair_prod(self, mat, not_el=None):
+        """Compute the product of the lower mat elements
 
-        return factors.view(-1, 1)
+        Args:
+            mat (torch.tensor): input matrix [..., N x N]
+            not_el (tuple(i,j), optional):
+                single element to exclude of the product.
+                Defaults to None.
+
+        Returns:
+            torch.tensor : value of the product
+        """
+
+        mat_cpy = mat.clone()
+        if not_el is not None:
+            i, j = not_el
+            mat_cpy[..., i, j] = 1
+
+        return mat_cpy[:, torch.tril(
+            torch.ones(self.nelec, self.nelec)) == 0].prod(1).view(-1, 1)
 
 
 if __name__ == "__main__":
@@ -159,5 +220,5 @@ if __name__ == "__main__":
 
     r = jastrow.edist(pos)
     dr = jastrow.edist(pos, derivative=1)
-
+    d2r = jastrow.edist(pos, derivative=2)
     dval = jastrow(pos, derivative=1)
