185a25a081b4b7c64d35f0db901c7404276dd06d
NicoRenaud <nicolas.gm.renaud@gmail.com>
2019-12-27 14:05:09 +0100

    test jastrow in trace trick kinetic

--- tests/test_trace_trick.py
+++ tests/test_trace_trick.py
@@ -13,7 +13,7 @@ def btrace(M):
 
 class OrbitalTest(Orbital):
     def __init__(self, mol):
-        super(OrbitalTest, self).__init__(mol, use_jastrow=False)
+        super(OrbitalTest, self).__init__(mol, use_jastrow=True)
 
     def first_der_autograd(self, x):
         """Compute the first derivative of the AO using autograd
@@ -277,10 +277,13 @@ class TestTrace(unittest.TestCase):
     def test_kinetic(self):
         """Test the values kinetic energy computed via autograd and
         trace trick."""
-        kin_auto = self.wf.kinetic_energy_autograd(self.x)
-        kin_trace = self.wf.kinetic_energy_jacobi(self.x)
+        kin_auto = self.wf.kinetic_energy_autograd(self.x)/self.wf(self.x)
+        kin_trace = self.wf.kinetic_energy_jacobi(
+            self.x, return_local_energy=True)
+        delta = kin_auto / kin_trace
 
-        assert torch.allclose(kin_auto.view(-1), kin_trace.view(-1))
+        assert torch.allclose(delta, torch.ones_like(
+            delta), atol=1e-5, rtol=1E-5)
 
 
 if __name__ == "__main__":
