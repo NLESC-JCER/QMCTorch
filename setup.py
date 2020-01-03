95fcd2a5eaa71b6d8e12d8712c370d8252dbc8cd
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-01-03 17:43:54 +0100

    added hdv in requirements

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setup(
     test_suite='tests',
     install_requires=['autograd', 'cython', 'matplotlib', 'numpy',
                       'pyyaml>=5.1', 'schema', 'scipy', 'tqdm', 'torch',
-                      'basis_set_exchange', 'pyscf', 'mendeleev'],
+                      'basis_set_exchange', 'pyscf', 'mendeleev',
+                      'hovorod'],
     extras_require={
         'dev': ['prospector[with_pyroma]', 'yapf', 'isort'],
         'doc': ['recommonmark', 'sphinx', 'sphinx_rtd_theme'],
