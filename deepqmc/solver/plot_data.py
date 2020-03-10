c3b381fc92b630d975b9184dc94abd978519fc62
local_renaud <renaud@ccpgate.tnw.utwente.nl>
2020-03-10 14:50:15 +0100

    baby plot

--- deepqmc/solver/plot_data.py
+++ deepqmc/solver/plot_data.py
@@ -61,19 +61,49 @@ def plot_observable(obs_dict, e0=None, ax=None, var=False):
 
 def plot_block(obs_dict):
 
-    eloc = np.array(obs_dict['local_energy']).squeeze()
+    eloc = np.array(obs_dict['local_energy']).squeeze(-1)
     nstep, nwalkers = eloc.shape
     block_size_max = nstep//2
 
     evar = []
     for size in range(1, block_size_max):
-        tmp = np.copy(eloc[:size*(nstep//size), :])
-        tmp = tmp.reshape(size, nstep//size, nwalkers).mean(axis=0)
-        evar.append(np.var(tmp))
-
+        nblock = nstep//size
+        tmp = np.copy(eloc[:size*nblock, :])
+        tmp = tmp.reshape(size, nblock, nwalkers).mean(axis=0)
+        evar.append(np.sqrt(np.var(tmp)/ (nblock-1)) )
+    np.savetxt('block.dat',np.array(evar))
+    print(evar)
     plt.plot(np.array(evar))
     plt.show()
 
+def plot_block_baby(obs_dict):
+
+    
+    m = np.array(obs_dict['local_energy'])
+    K = m.shape[0]
+    Keq = 0
+    Keff = K - Keq
+    nstepx = Keff//2
+    err = []
+    for i in range(1,nstepx):
+        nblock = Keff//i
+        iter = 0
+        mcum=0
+        mcm2=0
+        for j in range(1,nblock):
+            msum=0
+            for l in range(1,i):
+                iter=iter+1
+                msum=msum+m[iter+Keq]
+        
+        mcum=mcum+msum/i
+        mcm2=mcm2+msum*msum/i**2
+    
+    mcum=mcum/nblock;
+    mcm2=mcm2/nblock;
+    err.append(np.sqrt((mcm2-mcum**2)/(nblock-1)))
+    print(err)
+    np.savetxt('block.dat',np.array(err))
 
 def save_observalbe(filename, obs_dict):
     with open(filename, 'wb') as fhandle:
