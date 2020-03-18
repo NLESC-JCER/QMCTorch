6e897147af14b52c276e8fd5f389a6f0e0c3824b
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-03-18 16:05:05 +0100

    clean up molecule

--- deepqmc/wavefunction/molecule.py
+++ deepqmc/wavefunction/molecule.py
@@ -9,10 +9,7 @@ import json
 
 class Molecule(object):
 
-    def __init__(self, atom=None,
-                 basis_type='gto',
-                 basis='sto-3g',
-                 unit='bohr'):
+    def __init__(self, atom, basis_type, basis, unit='bohr'):
 
         self.atoms_str = atom
 
@@ -21,7 +18,6 @@ class Molecule(object):
             raise ValueError("basis_type must be sto or gto")
 
         self.basis = basis.lower()
-
         self.code_mo = {'gto': 'pyscf',
                         'sto': 'adf'}[self.basis_type]
 
@@ -38,7 +34,6 @@ class Molecule(object):
             raise ValueError('unit should be angs or bohr')
 
         self.process_atom_str()
-        self.get_bonds()
 
         # get the basis folder
         try:
@@ -62,18 +57,9 @@ class Molecule(object):
         self.mult_bas = {'S': 1, 'P': 3, 'D': 5}
 
         # get the m value of each orb type
-        # the ordering might be different in different
-        # quantum packages
-        if self.basis_type == 'gto':
-            self.get_m = {'S': [0],
-                          'P': [-1, 1, 0],
-                          'D': [-2, -1, 0, 1, 2]}
-        else:
-            self.get_m = {'S': [0],
-                          'P': [1, -1, 0],
-                          'D': [-2, -1, 0, 1, 2]}  # D unchecked so far
+        self.get_m = self.set_orbital_ordering()
 
-        # for cartesian
+        # for cartesian (used ?)
         self.get_lmn_cart = {'S': [0, 0, 0],
                              'P': [[1, 0, 0],
                                    [0, 1, 0],
@@ -88,25 +74,39 @@ class Molecule(object):
         # read/process basis info
         self.process_basis()
 
+    def set_orbital_ordering(self):
+        '''get the m values of the orbital for each code.'''
+
+        if self.code_mo == 'pyscf':
+            dict_m_val = {'S': [0],
+                          'P': [-1, 1, 0],
+                          'D': [-2, -1, 0, 1, 2]}
+
+        elif self.code_mo == 'adf':
+            dict_m_val = {'S': [0],
+                          'P': [1, -1, 0],
+                          'D': [-2, -1, 0, 1, 2]}
+        return dict_m_val
+
     def process_atom_str(self):
         '''Process the input file.'''
 
-        # if we have an xyz file
         if os.path.isfile(self.atoms_str):
-            with open(self.atoms_str, 'r') as f:
-                data = f.readlines()
-            atoms = data[2:]
-            self.atoms_str = ''
-            for a in atoms[:-1]:
-                self.atoms_str += a + '; '
-            self.atoms_str += atoms[-1]
-
-        # if we have a string
+            atoms = self._process_xyz_file()
         else:
             atoms = self.atoms_str.split(';')
 
-        # loop over all atoms
-        for a in atoms:
+        self.get_atomic_properties(atoms)
+
+    def get_atomic_properties(self, atoms):
+        """Get the properties of all atoms in the molecule
+
+        Arguments:
+            atoms {list} -- atoms and xyz position
+        """
+
+       # loop over all atoms
+       for a in atoms:
             atom_data = a.split()
             self.atoms.append(atom_data[0])
             x, y, z = float(atom_data[1]), float(
@@ -127,36 +127,99 @@ class Molecule(object):
         self.nup = math.ceil(self.nelec/2)
         self.ndown = math.floor(self.nelec/2)
 
-    def get_bonds(self):
-        self.bonds = list()
-        for iat1 in range(self.natom-1):
-            at1 = self.atoms[iat1]
-            xyz1 = np.array(self.atom_coords[iat1])
-            for iat2 in range(iat1+1, self.natom):
-                at2 = self.atoms[iat2]
-                xyz2 = np.array(self.atom_coords[iat2])
-
-                d = np.sqrt(np.sum((xyz1-xyz2)**2))
-                thr = self._get_max_blength(at1, at2)
-                if d < thr:
-                    self.bonds.append((iat1, iat2))
-
-    @staticmethod
-    def _get_max_blength(at1, at2):
-        bond = {('H', 'H'): 2.}
-        if (at1, at2) in bond:
-            return bond[(at1, at2)]
-        else:
-            return 2.
+    def _process_xyz_file(self):
+        """Process a xyz file containing the data
+        
+        Returns:
+            list -- atoms and xyz position
+        """
+        with open(self.atoms_str, 'r') as f:
+            data = f.readlines()
+        atoms = data[2:]
+        self.atoms_str = ''
+        for a in atoms[:-1]:
+            self.atoms_str += a + '; '
+        self.atoms_str += atoms[-1]
+        return atoms
 
     def process_basis(self):
+        """Extract the orbital information."""
+
         if self.basis_type == 'sto':
-            self._process_sto()
+            self.process_sto_basis()
 
         elif self.basis_type == 'gto':
-            self._process_gto()
+            self.process_gto_basis()
+
+    def process_sto_basis(self):
+        """Get the properties of all orbital in the molecule.
+        
+        Raises:
+            ValueError: if orbitals larger than D are used.
+        """
+
+        # number of orbs
+        self.norb = 0
+
+        # loop over all the atoms
+        for at in self.atoms:
+
+            data = self._get_sto_atomic_data(at)
+            self.nshells.append(0)
+
+            for ishell, shell in data['electron_shells'].items():
+
+                # primary quantum number
+                n = ishell
+
+                # loop over the angular momentum
+                for iangular, angular in enumerate(shell['angular_momentum']):
+
+                    # secondary qn and multiplicity
+                    l = angular
+                    if l > self.max_angular:
+                        raise ValueError('Only orbital up to l=%d (%s) are currently supported',
+                                         self.max_angular, self.get_label[self.max_angular])
+
+                    mult = self.mult_bas[self.get_label[angular]]
+                    nbas = len(shell['coefficients'][iangular])
+                    mvals = self.get_m[self.get_label[angular]]
+
+                    for imult in range(mult):
+
+                        # self.norb += 1
+                        self.norb += nbas
+
+                        # store coeffs and exps of the bas
+                        self.bas_exp += shell['exponents'][iangular]
+                        self.bas_coeffs += shell['coefficients'][iangular]
+
+                        # index of the contraction
+                        # adf does not contract but maybe other 
+                        # self.index_ctr += [self.norb-1] * nbas
+
+                        # store the quantum numbers
+                        self.bas_n += [n]*nbas
+                        self.bas_l += [l]*nbas
+                        self.bas_m += [mvals[imult]]*nbas
+
+                    # number of shells
+                    self.nshells[-1] += nbas*mult
+
+        self.index_ctr = list(range(self.norb))
 
     def _get_sto_atomic_data(self, at):
+        """Get the properties of an STO basis of a single atom
+
+        Arguments:
+            at {str} -- atom type
+
+        Raises:
+            ValueError: if orbital up thad D are used
+
+        Returns:
+            dict -- properties of the different orbital for the specified atom
+        """
 
         atomic_data = {'electron_shells': {}}
 
@@ -203,58 +266,8 @@ class Molecule(object):
 
         return atomic_data
 
-    def _process_sto(self):
-
-        # number of orbs
-        self.norb = 0
-
-        # loop over all the atoms
-        for at in self.atoms:
-
-            data = self._get_sto_atomic_data(at)
-            self.nshells.append(0)
-
-            for ishell, shell in data['electron_shells'].items():
-
-                # primary quantum number
-                n = ishell
-
-                # loop over the angular momentum
-                for iangular, angular in enumerate(shell['angular_momentum']):
-
-                    # secondary qn and multiplicity
-                    l = angular
-                    if l > self.max_angular:
-                        raise ValueError('Only orbital up to l=%d (%s) are currently supported',
-                                         self.max_angular, self.get_label[self.max_angular])
-
-                    mult = self.mult_bas[self.get_label[angular]]
-                    nbas = len(shell['coefficients'][iangular])
-                    mvals = self.get_m[self.get_label[angular]]
-
-                    for imult in range(mult):
-
-                        # self.norb += 1
-                        self.norb += nbas
-
-                        # store coeffs and exps of the bas
-                        self.bas_exp += shell['exponents'][iangular]
-                        self.bas_coeffs += shell['coefficients'][iangular]
-
-                        # index of the contraction
-                        # self.index_ctr += [self.norb-1] * nbas
-
-                        # store the quantum numbers
-                        self.bas_n += [n]*nbas
-                        self.bas_l += [l]*nbas
-                        self.bas_m += [mvals[imult]]*nbas
-
-                    # number of shells
-                    self.nshells[-1] += nbas*mult
-
-        self.index_ctr = list(range(self.norb))
-
-    def _process_gto(self):
+    def process_gto_basis(self):
+        """Get the properties of all the orbitals in the molecule."""
 
         # number of orbs
         self.norb = 0
@@ -306,9 +319,13 @@ class Molecule(object):
                     self.nshells[-1] += mult*nbas
 
     def get_mo_coeffs(self):
+        """Compute the molecular orbital of the molecule
+        
+        Returns:
+            np.ndarray -- molecular orbital matrix
+        """
 
         if self.basis_type == 'gto':
-
             if self.code_mo.lower() == 'pyscf':
                 mo = self._get_mo_pyscf()
             else:
@@ -316,8 +333,8 @@ class Molecule(object):
                     self.code_mo + 'not currently supported for GTO orbitals')
 
         elif self.basis_type == 'sto':
-
             if self.code_mo.lower() == 'pyscf':
+                print('Warning : using pyscf to evaluate STO SCF molecular orbitals !')
                 mo = self._get_mo_pyscf()
             elif self.code_mo.lower() == 'adf':
                 mo = self._get_mo_adf()
@@ -328,20 +345,36 @@ class Molecule(object):
         return mo
 
     def _get_mo_pyscf(self):
-
+        """Get the molecular orbital using pyscf.
+        
+        Returns:
+            np.ndarray -- molecular orbital matrix
+        """
         if self.basis_type == 'sto':
             bdict = {'sz': 'sto-3g', 'dz': 'sto-3g'}
             pyscf_basis = bdict[self.basis]
         else:
             pyscf_basis = self.basis
 
+        # refresh the atom positions if necessary
         self._get_atoms_str()
 
+        # pyscf calculation
         mol = gto.M(atom=self.atoms_str, basis=pyscf_basis, unit=self.unit)
         rhf = scf.RHF(mol).run()
+
+        # normalize the MO and return them
         return self._normalize_columns(rhf.mo_coeff)
 
     def _get_mo_adf(self):
+        """Get the molecular orbital using ADF/PLAMS.
+        
+        Raises:
+            ValueError: [description]
+        
+        Returns:
+            np.ndarray -- molecular orbital matrix
+        """
 
         from scm import plams
         import shutil
@@ -353,69 +386,91 @@ class Molecule(object):
         plams_wd = './plams_workdir'
         t21_path = os.path.join(plams_wd, os.path.join(wd, t21_name))
 
+        # if the target t21 already exists reads it
         if os.path.isfile(t21_name):
 
             kf = plams.KFFile(t21_name)
             nmo = kf.read('A', 'nmo_A')
             bas_mos = np.array(kf.read('A', mo_keys))
 
+        # perform the calculations
         else:
 
+            # init PLAMS
             plams.init()
             plams.config.log.stdout = -1
             plams.config.erase_workdir = True
 
+            # create the molecule
             mol = plams.Molecule()
             for at, xyz in zip(self.atoms, self.atom_coords):
                 mol.add_atom(plams.Atom(symbol=at, coords=tuple(xyz)))
 
+            # settings in PLAMS
             sett = plams.Settings()
             sett.input.basis.type = self.basis.upper()
             sett.input.basis.core = 'None'
             sett.input.symmetry = 'nosym'
             sett.input.XC.HartreeFock = ''
 
+            # correct unit
             if self.unit == 'angs':
                 sett.input.units.length = 'Angstrom'
-            elif self.unit == 'bohr':
+            else self.unit == 'bohr':
                 sett.input.units.length = 'Bohr'
-            else:
-                raise ValueError('unit should be angs or bohr')
 
+            # run the ADF job
             job = plams.ADFJob(molecule=mol, settings=sett, name=wd)
             job.run()
 
+            # read the data from the t21 file
             nmo = job.results.readkf('A', 'nmo_A')
             bas_mos = np.array(job.results.readkf('A', mo_keys))
 
+            # make a copy of the t21 file
             shutil.copyfile(t21_path, t21_name)
             shutil.rmtree(plams_wd)
 
+        # reshape/normalize and return MOs
         bas_mos = bas_mos.reshape(nmo, nmo).T
         return self._normalize_columns(bas_mos)
 
-        # mos = np.zeros((self.norb,self.norb))
-        # for ibas, imo in enumerate(self.index_ctr):
-        #     mos[imo,:] += bas_mos[ibas,:]
-        # return self._normalize_columns(mos)
-
     def _get_atoms_str(self):
+        """Refresh the atom string.  Necessary when atom positions have changed. """
         self.atoms_str = ''
         for iA in range(self.natom):
             self.atoms_str += self.atoms[iA] + ' '
             self.atoms_str += ' '.join(str(xi) for xi in self.atom_coords[iA])
             self.atoms_str += ';'
 
-    def update_mo_coeffs(self):
-        return self.get_mo_coeffs()
-
     @staticmethod
     def _normalize_columns(mat):
+        """Normalize a matrix column-wise.
+        
+        Arguments:
+            mat {np.ndarray} -- the matrix to be normalized
+        
+        Returns:
+            np.ndarray -- normalized matrix
+        """
         norm = np.sqrt((mat**2).sum(0))
         return mat / norm
 
     def domain(self, method):
-
+        """Define the walker initialization method
+        
+        Arguments:
+            method str -- 'center'  : all electron at the center of the molecule
+                          'uniform' : all electrons in a box covering the molecule
+                          'normal   : all electrons in a shpere covering the molecule 
+                          'atomic'  : electrons around atoms
+         
+        Raises:
+            ValueError: if method is not supported
+        
+        Returns:
+            dict -- data required to initialize the walkers
+        """
         domain = dict()
 
         if method == 'center':
@@ -436,6 +491,7 @@ class Molecule(object):
 
         else:
             raise ValueError('Method to initialize the walkers not recognized')
+
         return domain
 
 
