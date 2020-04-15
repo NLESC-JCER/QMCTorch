e5c212105c474333d1e0f9aeb1474268a4453a18
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-04-15 13:45:14 +0200

    test adf

---
+++ tests/test_h2_adf.py
@@ -0,0 +1,74 @@
+import torch
+import torch.optim as optim
+
+from qmctorch.wavefunction import Orbital, Molecule
+from qmctorch.solver import SolverOrbital
+from qmctorch.sampler import Metropolis, Hamiltonian
+
+
+import unittest
+
+
+class TestH2ADF(unittest.TestCase):
+
+    def setUp(self):
+
+        # molecule
+        self.mol = Molecule(
+            atom='H 0 0 -0.69; H 0 0 0.69',
+            unit='bohr',
+            calculator='adf',
+            basis='dzp')
+
+        # wave function
+        self.wf = Orbital(self.mol, kinetic='auto',
+                          configs='single(2,2)',
+                          use_jastrow=True)
+
+        # sampler
+        self.sampler = Metropolis(
+            nwalkers=1000,
+            nstep=2000,
+            step_size=0.5,
+            ndim=self.wf.ndim,
+            nelec=self.wf.nelec,
+            init=self.mol.domain('normal'),
+            move={
+                'type': 'all-elec',
+                'proba': 'normal'})
+
+        # optimizer
+        self.opt = optim.Adam(self.wf.parameters(), lr=0.01)
+
+        # solver
+        self.solver = SolverOrbital(wf=self.wf, sampler=self.sampler,
+                                    optimizer=self.opt)
+
+        # ground state energy
+        self.ground_state_energy = -1.16
+
+        # ground state pos
+        self.ground_state_pos = 0.69
+
+    def test_single_point(self):
+
+        self.solver.wf.ao.atom_coords[0, 2] = -self.ground_state_pos
+        self.solver.wf.ao.atom_coords[1, 2] = self.ground_state_pos
+        self.solver.sampler = self.sampler
+
+        # sample and compute observables
+        _, e, v = self.solver.single_point()
+
+        print('Energy   :', e)
+        print('Variance :', v)
+
+        # assert(e>self.ground_state_energy and e<-1.)
+        assert(e > 2 * self.ground_state_energy and e < 0.)
+        assert(v > 0 and v < 5.)
+
+
+if __name__ == "__main__":
+    # unittest.main()
+    t = TestH2ADF()
+    t.setUp()
+    t.test_single_point()
