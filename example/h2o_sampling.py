d1a18ff39a7dd16de627095e62271816bf30bb92
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-04-29 13:56:35 +0200

    example

---
+++ example/h2o_sampling.py
@@ -0,0 +1,34 @@
+from qmctorch.wavefunction import Orbital, Molecule
+from qmctorch.sampler import Metropolis
+from qmctorch.solver import SolverOrbital
+from qmctorch.utils import plot_walkers_traj
+
+# define the molecule
+mol = Molecule(atom='water.xyz', unit='angs',
+               calculator='pyscf', basis='sto-3g', name='water')
+
+# define the wave function
+wf = Orbital(mol, kinetic='jacobi',
+             configs='ground_State',
+             use_jastrow=True)
+
+# sampler
+sampler = Metropolis(nwalkers=100, nstep=300, step_size=0.25,
+                     nelec=wf.nelec, ndim=wf.ndim,
+                     init=mol.domain('atomic'),
+                     move={'type': 'one-elec', 'proba': 'normal'})
+
+# solver
+solver = SolverOrbital(wf=wf, sampler=sampler)
+
+# single point
+obs = solver.single_point()
+
+# reconfigure sampler
+solver.sampler.ntherm = 0
+solver.sampler.ndecor = 3
+
+# compute the sampling traj
+pos = solver.sampler(solver.wf.pdf)
+obs = solver.sampling_traj(pos)
+plot_walkers_traj(obs.local_energy, walkers='mean')
