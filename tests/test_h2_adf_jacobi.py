b01b07c9cb5326520d8c38c562c3e81ae32dc825
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-06-09 10:41:56 +0200

    test adf jacobi h2

--- tests/test_h2_adf_jacobi.py
+++ tests/test_h2_adf_jacobi.py
@@ -61,14 +61,14 @@ class TestH2ADFJacobi(unittest.TestCase):
         print(e.data.item(), v.data.item())
 
         # vals on different archs
-        # expected_energy = [-1.1571345329284668,
-        #                    -1.1501641653648578]
+        expected_energy = [-1.1571345329284668,
+                           -1.1501641653648578]
 
-        # expected_variance = [0.05087674409151077,
-        #                      0.05094174843043177]
+        expected_variance = [0.05087674409151077,
+                             0.05094174843043177]
 
-        # assert(np.any(np.isclose(e.data.item(), np.array(expected_energy))))
-        # assert(np.any(np.isclose(v.data.item(), np.array(expected_variance))))
+        assert(np.any(np.isclose(e.data.item(), np.array(expected_energy))))
+        assert(np.any(np.isclose(v.data.item(), np.array(expected_variance))))
 
     def test_wf_opt_auto_grad(self):
 
