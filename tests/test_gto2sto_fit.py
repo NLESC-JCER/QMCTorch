4dc52dc7cae809a85d3c9db77ab33b133136d49b
NicoRenaud <nicolas.gm.renaud@gmail.com>
2020-07-13 08:54:43 +0200

    add test for gto2sto fit

---
+++ tests/test_gto2sto_fit.py
@@ -0,0 +1,50 @@
+from qmctorch.wavefunction import Molecule, Orbital
+from qmctorch.utils import set_torch_double_precision
+import numpy as np
+import torch
+import unittest
+import itertools
+import os
+
+
+class TestGTO2STOFit(unittest.TestCase):
+
+    def setUp(self):
+
+        torch.manual_seed(101)
+        np.random.seed(101)
+
+        set_torch_double_precision()
+
+        # molecule
+        mol = Molecule(
+            atom='C 0 0 0',
+            unit='bohr',
+            calculator='pyscf',
+            basis='sto-3g',
+            redo_scf=True)
+
+        self.wf = Orbital(mol, kinetic='auto',
+                          configs='ground_state').gto2sto()
+
+        self.pos = -0.25 + 0.5*torch.tensor(np.random.rand(10, 18))
+        self.pos.requires_grad = True
+
+    def test_forward(self):
+
+        wfvals = self.wf(self.pos)
+        ref = torch.tensor([[-8.4430e-06],
+                            [1.5092e-02],
+                            [3.3809e-03],
+                            [9.7981e-03],
+                            [-6.8513e-02],
+                            [-4.6836e-03],
+                            [-3.2847e-04],
+                            [2.3636e-02],
+                            [5.5934e-04],
+                            [1.3205e-02]])
+        assert torch.allclose(wfvals.data, ref, rtol=1E-4, atol=1E-4)
+
+
+if __name__ == "__main__":
+    unittest.main()
