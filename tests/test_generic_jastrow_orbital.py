fix first derivative of the generic jastrows

tests/test_generic_jastrow_orbital.py
@@ -1,10 +1,15 @@
+import torch
+from torch.autograd import grad
+
+
 import unittest
 
 import numpy as np
 import torch
 from torch.autograd import Variable, grad, gradcheck
 
-from qmctorch.wavefunction.jastrows.pade_jastrow_orbital import PadeJastrowOrbital
+from qmctorch.wavefunction.jastrows.generic_jastrow_orbital import GenericJastrowOrbitals
+from qmctorch.wavefunction.jastrows.generic_jastrow_orbital import FullyConnectedJastrow
 
 torch.set_default_tensor_type(torch.DoubleTensor)
 
@@ -33,7 +38,7 @@ def hess(out, pos):
     return hess
 
 
-class TestPadeJastrowOrbital(unittest.TestCase):
+class TestGenericJastrowOrbital(unittest.TestCase):
 
     def setUp(self):
 
@@ -42,36 +47,20 @@ class TestPadeJastrowOrbital(unittest.TestCase):
 
         self.nup, self.ndown = 4, 4
         self.nelec = self.nup + self.ndown
-        self.nmo = 8
-        self.jastrow = PadeJastrowOrbital(
-            self.nup, self.ndown, self.nmo)
+        self.nmo = 10
+        self.jastrow = GenericJastrowOrbitals(
+            self.nup, self.ndown, self.nmo, FullyConnectedJastrow)
         self.nbatch = 5
 
-        self.pos = torch.rand(self.nbatch, self.nelec * 3)
+        self.pos = 1E-3 * torch.rand(self.nbatch, self.nelec * 3)
         self.pos.requires_grad = True
 
     def test_jastrow(self):
+        """simply checks that the values are not crashing."""
         val = self.jastrow(self.pos)
-
-    def test_jacobian_jastrow(self):
-
-        val = self.jastrow(self.pos)
-        dval = self.jastrow(self.pos, derivative=1)
-
-        dval_grad = grad(
-            val,
-            self.pos,
-            grad_outputs=torch.ones_like(val))[0]
-
-        dval_grad = dval_grad.view(
-            self.nbatch, self.nelec, 3).sum(2)
-        gradcheck(self.jastrow, self.pos)
-
-        assert torch.allclose(dval.sum(0), dval_grad)
-        assert(torch.allclose(dval.sum(), dval_grad.sum()))
 
     def test_grad_jastrow(self):
-
+        """Checks the values of the gradients."""
         val = self.jastrow(self.pos)
         dval = self.jastrow(self.pos, derivative=1, jacobian=False)
 
@@ -81,11 +70,33 @@ class TestPadeJastrowOrbital(unittest.TestCase):
             grad_outputs=torch.ones_like(val))[0]
 
         dval_grad = dval_grad.reshape(
-            self.nbatch, self.nelec, 3).permute(0,2,1)
+            self.nbatch, self.nelec, 3).permute(0, 2, 1)
+
+        gradcheck(self.jastrow, self.pos)
+
+        # Warning : using grad on a model made out of ModuleList
+        # automatically summ the values of the grad of the different
+        # modules in the list !
+        assert(torch.allclose(dval.sum(), dval_grad.sum()))
+        assert(torch.allclose(dval.sum(0), dval_grad))
+
+    def test_jacobian_jastrow(self):
+        """Checks the values of the gradients."""
+        val = self.jastrow(self.pos)
+        dval = self.jastrow(self.pos, derivative=1)
+
+        dval_grad = grad(
+            val,
+            self.pos,
+            grad_outputs=torch.ones_like(val))[0]
+
+        dval_grad = dval_grad.reshape(
+            self.nbatch, self.nelec, 3).permute(0, 2, 1).sum(-2)
+
         gradcheck(self.jastrow, self.pos)
 
         assert(torch.allclose(dval.sum(), dval_grad.sum()))
-        assert(torch.allclose(dval.sum(0), dval_grad))
+        assert torch.allclose(dval.sum(0), dval_grad)
 
     def test_hess_jastrow(self):
 
@@ -99,8 +110,9 @@ class TestPadeJastrowOrbital(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    t = TestPadeJastrowOrbital()
+    t = TestGenericJastrowOrbital()
     t.setUp()
     t.test_jastrow()
     t.test_grad_jastrow()
-    t.test_hess_jastrow()
+    t.test_jacobian_jastrow()
+    # t.test_hess_jastrow()