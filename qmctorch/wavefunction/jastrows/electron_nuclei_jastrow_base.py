95e809d022618600711cdf5b6b3c29e18e21fd01
NicoRenaud <nicolas.gm.renaud@gmail.com>
2021-02-16 09:10:22 +0100

    started elec-nuc jastrow

---
+++ qmctorch/wavefunction/jastrows/electron_nuclei_jastrow_base.py
@@ -0,0 +1,153 @@
+import torch
+from torch import nn
+from .electron_nuclei_distance import ElectronNucleiDistance
+import itertools
+
+
+class ElectronNucleiJastrowFactorBase(nn.Module):
+
+    def __init__(self, nup, ndown, atomic_pos, cuda=False):
+        r"""Base class for two el-nuc jastrow of the form:
+
+        .. math::
+            J = \prod_{a,i} \exp(A(r_{ai}))
+
+        Args:
+            nup (int): number of spin up electons
+            ndow (int): number of spin down electons
+            atomic_pos (tensor): positions of the atoms
+            cuda (bool, optional): Turns GPU ON/OFF. Defaults to False.
+        """
+
+        super(ElectronNucleiJastrowFactorBase, self).__init__()
+
+        self.nup = nup
+        self.ndown = ndown
+        self.nelec = nup + ndown
+        self.atoms = atomic_pos
+        self.ndim = 3
+
+        self.cuda = cuda
+        self.device = torch.device('cpu')
+        if self.cuda:
+            self.device = torch.device('cuda')
+
+        self.edist = ElectronNucleiDistance(
+            self.nelec, self.atoms, self.ndim)
+
+    def forward(self, pos, derivative=0, jacobian=True):
+        """Compute the Jastrow factors.
+
+        Args:
+            pos (torch.tensor): Positions of the electrons
+                                  Size : Nbatch, Nelec x Ndim
+            derivative (int, optional): order of the derivative (0,1,2,).
+                            Defaults to 0.
+            jacobian (bool, optional): Return the jacobian (i.e. the sum of
+                                       the derivatives) or the individual
+                                       terms. Defaults to True.
+                                       False only for derivative=1
+
+        Returns:
+            torch.tensor: value of the jastrow parameter for all confs
+                          derivative = 0  (Nmo) x Nbatch x 1
+                          derivative = 1  (Nmo) x Nbatch x Nelec (for jacobian = True)
+                          derivative = 1  (Nmo) x Nbatch x Ndim x Nelec (for jacobian = False)
+                          derivative = 2  (Nmo) x Nbatch x Nelec
+        """
+
+        size = pos.shape
+        assert size[1] == self.nelec * self.ndim
+        nbatch = size[0]
+
+        r = self.edist(pos)
+        jast = self._get_jastrow_elements(r)
+
+        if derivative == 0:
+            return jast.prod(-1).prod(-1).unsqueeze(-1)
+
+        elif derivative == 1:
+            dr = self.extract_tri_up(self.edist(
+                pos, derivative=1)).view(nbatch, 3, -1)
+            return self._jastrow_derivative(r, dr, jast, jacobian)
+
+        elif derivative == 2:
+
+            dr = self.extract_tri_up(self.edist(
+                pos, derivative=1)).view(nbatch, 3, -1)
+            d2r = self.extract_tri_up(self.edist(
+                pos, derivative=2)).view(nbatch, 3, -1)
+
+            return self._jastrow_second_derivative(r, dr, d2r, jast)
+
+        elif derivative == [0, 1, 2]:
+
+            dr = self.extract_tri_up(self.edist(
+                pos, derivative=1)).view(nbatch, 3, -1)
+            d2r = self.extract_tri_up(self.edist(
+                pos, derivative=2)).view(nbatch, 3, -1)
+
+            return(jast.prod(-1).unsqueeze(-1),
+                   self._jastrow_derivative(r, dr, jast, jacobian),
+                   self._jastrow_second_derivative(r, dr, d2r, jast))
+
+    def _get_jastrow_elements(self, r):
+        r"""Get the elements of the jastrow matrix :
+        .. math::
+            out_{i,j} = \exp{ U(r_{ij}) }
+
+        Args:
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+
+        Returns:
+            torch.tensor: matrix fof the jastrow elements
+                          Nbatch x Nelec x Nelec
+        """
+        raise NotImplementedError('Jastrow element not implemented')
+
+    def _get_der_jastrow_elements(self, r, dr):
+        """Get the elements of the derivative of the jastrow kernels
+        wrt to the first electrons
+
+        .. math::
+
+            d B_{ij} / d k_i =  d B_{ij} / d k_j  = - d B_{ji} / d k_i
+
+        Args:
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+            dr (torch.tensor): matrix of the derivative of the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+
+        Returns:
+            torch.tensor: matrix fof the derivative of the jastrow elements
+                          Nbatch x Ndim x Nelec x Nelec
+        """
+        raise NotImplementedError(
+            'Jastrow derivative not implemented')
+
+    def _get_second_der_jastrow_elements(self, r, dr, d2r):
+        """Get the elements of the pure 2nd derivative of the jastrow kernels
+        wrt to the first electron
+
+        .. math ::
+
+            d^2 B_{ij} / d k_i^2 =  d^2 B_{ij} / d k_j^2 = d^2 B_{ji} / d k_i^2
+
+        Args:
+            r (torch.tensor): matrix of the e-e distances
+                              Nbatch x Nelec x Nelec
+            dr (torch.tensor): matrix of the derivative of the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+            d2r (torch.tensor): matrix of the 2nd derivative of
+                                the e-e distances
+                              Nbatch x Ndim x Nelec x Nelec
+
+        Returns:
+            torch.tensor: matrix fof the pure 2nd derivative of
+                          the jastrow elements
+                          Nbatch x Ndim x Nelec x Nelec
+        """
+        raise NotImplementedError(
+            'Jastrow second derivative not implemented')
