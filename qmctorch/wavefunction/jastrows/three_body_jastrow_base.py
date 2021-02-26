fix first derivative of 3body jastrow

qmctorch/wavefunction/jastrows/three_body_jastrow_base.py
@@ -35,7 +35,7 @@ class ThreeBodyJastrowFactorBase(nn.Module):
 
         self.mask_tri_up, self.index_col, self.index_row = self.get_mask_tri_up()
         self.index_elec = [
-            self.index_col.tolist(), self.index_row.tolist()]
+            self.index_row.tolist(), self.index_col.tolist()]
 
         self.elel_dist = ElectronDistance(self.nelec, self.ndim)
         self.elnu_dist = ElectronNucleiDistance(
@@ -159,7 +159,7 @@ class ThreeBodyJastrowFactorBase(nn.Module):
         r"""Organize the elec nuc distances
 
         Args:
-            en_dist (torch.tensor): electron-nuclei distances 
+            en_dist (torch.tensor): electron-nuclei distances
                                     nbatch x nelec x natom or
                                     nbatch x 3 x nelec x natom (dr)
 
@@ -300,29 +300,57 @@ class ThreeBodyJastrowFactorBase(nn.Module):
         nbatch = r.shape[0]
         if jacobian:
 
-            prod_val = jast.view(nbatch, -1).prod(-1).unsqueeze(-1)
-            djast = self._get_der_jastrow_elements(r, dr).sum(-2)
-            print(djast.shape)
-            print(prod_val.shape)
-            djast = djast * prod_val
+            prod_val = jast.view(nbatch, -1).prod(-1)
 
-            # might cause problems with backward cause in place operation
-            out_shape = list(djast.shape[:-1]) + [self.nelec]
+            # derivative of the jastrow elements
+            # nbatch x ndim x natom x nelec_pair x 3
+            # last dim is (ria rja rij)
+            djast = self._get_der_jastrow_elements(r, dr)
+
+            # sum dim and atom
+            djast = djast.sum([1, 2])
+
+            # multiply with the product of jastrow el values
+            djast = djast * prod_val.unsqueeze(-1).unsqueeze(-1)
+
+            # create the output vector with size nbatch x nelec
+            out_shape = list(djast.shape[:-2]) + [self.nelec]
             out = torch.zeros(out_shape).to(self.device)
-            out.index_add_(-1, self.index_row, djast)
-            out.index_add_(-1, self.index_col, -djast)
+
+            # add the elec-elec term
+            out.index_add_(-1, self.index_row, djast[..., 2])
+            out.index_add_(-1, self.index_col, -djast[..., 2])
+
+            # add the elec-nuc terms
+            out.index_add_(-1, self.index_row, djast[..., 0])
+            out.index_add_(-1, self.index_col, djast[..., 1])
 
         else:
 
-            prod_val = jast.prod(-1).unsqueeze(-1).unsqueeze(-1)
+            # product of jastrow terms
+            prod_val = jast.view(nbatch, -1).prod(-1)
+
+            # derivative of the jastrow elements
+            # nbatch x ndim x natom x nelec_pair x 3
+            # last dim is (ria rja rij)
             djast = self._get_der_jastrow_elements(r, dr)
-            djast = djast * prod_val
+
+            # sum atom
+            djast = djast.sum(2)
+            djast = djast * \
+                prod_val.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
 
             # might cause problems with backward cause in place operation
-            out_shape = list(djast.shape[:-1]) + [self.nelec]
+            out_shape = list(djast.shape[:-2]) + [self.nelec]
             out = torch.zeros(out_shape).to(self.device)
-            out.index_add_(-1, self.index_row, djast)
-            out.index_add_(-1, self.index_col, -djast)
+
+            # add electronic terms
+            out.index_add_(-1, self.index_row, djast[..., 2])
+            out.index_add_(-1, self.index_col, -djast[..., 2])
+
+            # add elec-nuc terms
+            out.index_add_(-1, self.index_row, djast[..., 0])
+            out.index_add_(-1, self.index_col, djast[..., 1])
 
         return out
 

tests/test_pade_jastrow.py
@@ -38,7 +38,7 @@ class TestPadeJastrow(unittest.TestCase):
         torch.manual_seed(0)
         np.random.seed(0)
 
-        self.nup, self.ndown = 4, 4
+        self.nup, self.ndown = 1, 1
         self.nelec = self.nup + self.ndown
         self.jastrow = PadeJastrow(self.nup, self.ndown)
         self.nbatch = 5
@@ -86,4 +86,13 @@ class TestPadeJastrow(unittest.TestCase):
 
 
 if __name__ == "__main__":
-    unittest.main()
+    # unittest.main()
+
+    nup, ndown = 2, 2
+    nelec = nup + ndown
+
+    jastrow = PadeJastrow(nup, ndown)
+    nbatch = 1
+
+    pos = torch.rand(nbatch, nelec * 3)
+    pos.requires_grad = True

tests/test_three_body_jastrow_generic.py
@@ -86,7 +86,8 @@ class TestThreeBodyGeneric(unittest.TestCase):
 
         self.nup, self.ndown = 4, 4
         self.nelec = self.nup + self.ndown
-        self.atoms = torch.rand(4, 3)
+        self.natom = 4
+        self.atoms = torch.rand(self.natom, 3)
         self.jastrow = ThreeBodyJastrowFactorGeneric(
             self.nup, self.ndown, self.atoms, FullyConnected, False)
         self.nbatch = 5
@@ -94,7 +95,7 @@ class TestThreeBodyGeneric(unittest.TestCase):
         self.pos = torch.rand(self.nbatch, self.nelec * 3)
         self.pos.requires_grad = True
 
-    def test_grad_distance(self):
+    def test_grad_elel_distance(self):
 
         r = self.jastrow.elel_dist(self.pos)
         dr = self.jastrow.elel_dist(self.pos, derivative=1)
@@ -103,9 +104,26 @@ class TestThreeBodyGeneric(unittest.TestCase):
             self.pos,
             grad_outputs=torch.ones_like(r))[0]
 
-        assert(torch.allclose(dr.sum(), dr_grad.sum(), atol=1E-5))
+        dr_grad = dr_grad.reshape(self.nbatch, self.nelec, 3)
+        dr = dr.sum(-1).permute(0, 2, 1)
 
-    def test_grad_jastrow(self):
+        assert(torch.allclose(2*dr, dr_grad, atol=1E-5))
+
+    def test_grad_elnu_distance(self):
+
+        r = self.jastrow.elnu_dist(self.pos)
+        dr = self.jastrow.elnu_dist(self.pos, derivative=1)
+        dr_grad = grad(
+            r,
+            self.pos,
+            grad_outputs=torch.ones_like(r))[0]
+
+        dr_grad = dr_grad.reshape(self.nbatch, self.nelec, 3)
+        dr = dr.sum(-1).permute(0, 2, 1)
+
+        assert(torch.allclose(dr, dr_grad, atol=1E-5))
+
+    def test_jacobian_jastrow(self):
 
         val = self.jastrow(self.pos)
         dval = self.jastrow(self.pos, derivative=1)
@@ -117,41 +135,62 @@ class TestThreeBodyGeneric(unittest.TestCase):
         dval_grad = dval_grad.view(
             self.nbatch, self.nelec, 3).sum(2)
 
-        assert torch.allclose(dval, dval_grad)
         assert(torch.allclose(dval.sum(), dval_grad.sum()))
+        assert torch.allclose(dval, dval_grad)
 
-    def test_hess_jastrow(self):
+    def test_grad_jastrow(self):
 
         val = self.jastrow(self.pos)
-        d2val_grad = hess(val, self.pos)
-        d2val = self.jastrow(self.pos, derivative=2)
+        dval = self.jastrow(self.pos, derivative=1, jacobian=False)
+        dval_grad = grad(
+            val,
+            self.pos,
+            grad_outputs=torch.ones_like(val))[0]
 
-        assert torch.allclose(d2val, d2val_grad.view(
-            self.nbatch, self.nelec, 3).sum(2))
-        assert(torch.allclose(d2val.sum(), d2val_grad.sum()))
+        dval_grad = dval_grad.view(
+            self.nbatch, self.nelec, 3)
+
+        assert(torch.allclose(dval.sum(), dval_grad.sum()))
+        assert torch.allclose(dval.permute(0, 2, 1), dval_grad)
+
+    # def test_hess_jastrow(self):
+
+    #     val = self.jastrow(self.pos)
+    #     d2val_grad = hess(val, self.pos)
+    #     d2val = self.jastrow(self.pos, derivative=2)
+
+    #     assert torch.allclose(d2val, d2val_grad.view(
+    #         self.nbatch, self.nelec, 3).sum(2))
+    #     assert(torch.allclose(d2val.sum(), d2val_grad.sum()))
 
 
 if __name__ == "__main__":
     # unittest.main()
-    t = TestThreeBodyGeneric()
-    t.setUp()
-    t.test_grad_distance()
-    t.test_grad_jastrow()
-    # nup, ndown = 4, 4
-    # nelec = nup + ndown
-    # atoms = torch.rand(4, 3)
-    # jastrow = ThreeBodyJastrowFactorGeneric(
-    #     nup, ndown, atoms, FullyConnected, False)
-    # nbatch = 5
+    if 1:
+        t = TestThreeBodyGeneric()
+        t.setUp()
+        t.test_grad_elel_distance()
+        t.test_grad_elnu_distance()
+        t.test_jacobian_jastrow()
+        t.test_grad_jastrow()
 
-    # pos = torch.rand(nbatch, nelec * 3)
-    # pos.requires_grad = True
+    if 1:
+        nup, ndown = 4, 4
+        nelec = nup + ndown
+        atoms = torch.rand(4, 3)
+        jastrow = ThreeBodyJastrowFactorGeneric(
+            nup, ndown, atoms, FullyConnected, False)
+        nbatch = 5
 
-    # ree = jastrow.extract_tri_up(jastrow.elel_dist(pos))
-    # ren = jastrow.extract_elec_nuc_dist(jastrow.elnu_dist(pos))
-    # r = jastrow.assemble_dist(pos)
+        pos = torch.rand(nbatch, nelec * 3)
+        pos.requires_grad = True
 
-    # val = jastrow.jastrow_function(r)
-    # gval = jastrow._grads(val, r)
+        ree = jastrow.extract_tri_up(jastrow.elel_dist(pos))
+        ren = jastrow.extract_elec_nuc_dist(jastrow.elnu_dist(pos))
+        r = jastrow.assemble_dist(pos)
 
-    # dr = jastrow.assemble_dist_deriv(pos)
+        jast = jastrow._get_jastrow_elements(r)
+        jast = jast.view(nbatch, -1).prod(-1).unsqueeze(-1)
+
+        dr = jastrow.assemble_dist_deriv(pos)
+        djast = jastrow._get_der_jastrow_elements(r, dr)