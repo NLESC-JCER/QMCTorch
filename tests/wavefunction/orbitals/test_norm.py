7f78121b69934ef40babebc35b35702259c327a9
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-04-06 13:39:10 +0200

    tensor->as_tensor

--- tests/wavefunction/orbitals/test_norm.py
+++ tests/wavefunction/orbitals/test_norm.py
@@ -12,7 +12,7 @@ class TestAtomicOrbitalNorm(unittest.TestCase):
         basis = SimpleNamespace()
         basis.harmonics_type = 'sph'
         basis.radial_type = 'sto'
-        basis.bas_n = torch.tensor([0, 1, 2])
+        basis.bas_n = torch.as_tensor([0, 1, 2])
         basis.bas_exp = torch.rand(3)
 
         atomic_orbital_norm(basis)
@@ -22,7 +22,7 @@ class TestAtomicOrbitalNorm(unittest.TestCase):
         basis = SimpleNamespace()
         basis.harmonics_type = 'sph'
         basis.radial_type = 'gto'
-        basis.bas_n = torch.tensor([0, 1, 2])
+        basis.bas_n = torch.as_tensor([0, 1, 2])
         basis.bas_exp = torch.rand(3)
 
         atomic_orbital_norm(basis)
