fix second derivative of bf transformation

qmctorch/wavefunction/orbitals/atomic_orbitals_backflow.py
@@ -414,12 +414,38 @@ class AtomicOrbitalsBackFlow(AtomicOrbitals):
 
         # second derivative of the back flow kernel : d2 eta(r_ij)/d2 r_ij
         # Nbatch x 1 x Nelec x Nelec
-        dbf = self._backflow_kernel_second_derivative(
+        d2bf = self._backflow_kernel_second_derivative(
             ree).unsqueeze(1)
+
+        # (d^2 eta(r_ij) / d r_ij^2) (d r_ij/d x_i)^2
+        # + (d eta(r_ij) / d r_ij) (d^2 r_ij/d x_i^2)
+        # Nbatch x 3 x Nelec x Nelec
+        d2bf = (d2bf * dree * dree) + (dbf * d2ree)
 
         # (d eta(r_ij) / d r_ij) (d r_ij/d x_i)
         # Nbatch x 3 x Nelec x Nelec
         dbf = dbf * dree
+
+        # reshape the pos to : Nbatch x 3 x Nelec
+        pos = pos.reshape(nbatch, nelec, 3).permute(
+            0, 2, 1)
+
+        # (d eta(r_ij) / d r_ij) (d r_ij/d x_i) x_i
+        # Nbatch x 3 x Nelec x Nelec
+        d2bf_x = d2bf * pos.unsqueeze(-1)
+
+        # compute the diagonal terms
+        # Nbatch x Ndim x Nelec
+        # not sure of -1 or -2 dim ...
+        out = d2bf_x.sum(-2) + 2 * dbf.sum(-2)
+
+        # Nbatch x Ndim x Nelec x Nelec
+        out = torch.diag_embed(out, dim1=-1, dim2=-2)
+
+        # add the off diagonal term :
+        # - (d2 bf_ij / d2x_j) x_j - 2 (dbf_ij/d x_j)
+        # Nbatch x Ndim x Nelec x Nelec
+        return out - d2bf * pos.unsqueeze(-2) + 2 * dbf
 
     def _backflow_kernel(self, ree):
         """Computes the backflow function:
@@ -471,53 +497,3 @@ class AtomicOrbitalsBackFlow(AtomicOrbitals):
         eye = torch.eye(self.nelec, self.nelec).to(self.device)
         invree = (1./(ree+eye) - eye)
         return 2 * self.backflow_weight * invree * invree * invree
-
-    # def _backflow(self, pos):
-    #     """transform the position of the electrons
-
-    #     Args:
-    #         pos (torch.tensor): original positions Nbatch x [Nelec*Ndim]
-
-    #     Returns:
-    #         torch.tensor: transformed positions Nbatch x [Nelec*Ndim]
-    #     """
-
-    #     # reshape to have Nbatch x Nelec x Ndim
-    #     bf_pos = pos.reshape(-1, self.nelec, self.ndim)
-
-    #     # permute to Nbatch x Ndim x Nelec
-    #     bf_pos = bf_pos.permute(0, 2, 1)
-
-    #     # transform
-    #     bf_pos = bf_pos @ self.backflow_weights.T
-
-    #     # return with correct size : Nbatch x [Nelec*Ndim]
-    #     return bf_pos.permute(0, 2, 1).reshape(-1, self.nelec*self.ndim)
-
-    # def _backflow_derivative(self, pos):
-    #     r"""Computes the derivative of the back flow elec-orb distances
-    #        wrt the initial positions of the electrons
-
-    #     .. math::
-    #         \frac{d q_i^a}{d x_k} = \frac{d q_i^a}{d \tilde{x}_i} \frac{d \tilde{x}_i}{d x_k}
-    #         \\text{$q_i^a$ is the distance between bf elec i and orbital a}
-    #         q_i^a \sqrt{ (\tilde{x}_i-x_a)^2 + (\tilde{y}_i-y_a)^2 + (\tilde{z}_i-z_a)^2}
-    #         \\text{$\tilde{x}_i$ is the coordinate of bf elec i}
-    #         \\text{$x_k$ is the original coordinate of elec k}
-
-    #         \frac{d q_i^a}{d x_k} = \frac{\tilde{x}_i}{q_i^a} W[i,k]
-
-    #     Args:
-    #         pos (torch.tensor): orginal positions of the electrons Nbatch x [Nelec*Ndim]
-
-    #     Returns:
-    #         torch.tensor: d q_{ij}/d x_k  with :
-    #                       q_{ij} the distance between bf elec i and orb j
-    #                       x_k original coordinate of the kth elec
-    #                       Nelec x  Nbatch x Nelec x Norb x Ndim
-    #     """
-
-    #     xyz, r = self._process_position(pos)
-    #     der = (xyz/r.unsqueeze(-1)).permute(0, 2, 3, 1)
-    #     der = der[..., None] * self.backflow_weights
-    #     return der.permute(3, 0, 4, 1, 2)

tests/wavefunction/orbitals/test_backflow_ao_derivatives_pyscf.py
@@ -11,6 +11,7 @@ torch.set_default_tensor_type(torch.DoubleTensor)
 
 
 def hess(out, pos):
+
     # compute the jacobian
     z = Variable(torch.ones(out.shape))
     jacob = grad(out, pos,
@@ -34,6 +35,29 @@ def hess(out, pos):
     return hess
 
 
+def hess_single_element(out, inp):
+
+    shape = out.shape
+    out = out.reshape(-1, 1)
+
+    # compute the jacobian
+    z = Variable(torch.ones(out.shape))
+    jacob = grad(out, inp,
+                 grad_outputs=z,
+                 only_inputs=True,
+                 create_graph=True)[0]
+
+    # compute the diagonal element of the Hessian
+    z = Variable(torch.ones(jacob.shape))
+
+    hess = grad(jacob, inp,
+                grad_outputs=z,
+                only_inputs=True,
+                create_graph=True)[0]
+
+    return hess.reshape(*shape)
+
+
 class TestBFAOderivativesPyscf(unittest.TestCase):
 
     def setUp(self):
@@ -55,22 +79,37 @@ class TestBFAOderivativesPyscf(unittest.TestCase):
         self.pos = Variable(self.pos)
         self.pos.requires_grad = True
 
-    def test_backflow_kernel(self):
-        """Test the derivative of the kernel function wrt the elec-elec distance."""
+    def test_derivative_backflow_kernel(self):
+        """Test the derivative of the kernel function
+            wrt the elec-elec distance."""
 
         ree = self.ao.edist(self.pos)
-        bfpos = self.ao._backflow_kernel(ree)
+        bf_kernel = self.ao._backflow_kernel(ree)
 
-        dbfpos_grad = grad(
-            bfpos, ree, grad_outputs=torch.ones_like(bfpos))[0]
+        dbf_kernel_auto = grad(
+            bf_kernel, ree, grad_outputs=torch.ones_like(bf_kernel))[0]
 
-        dbfpos = self.ao._backflow_kernel_derivative(ree)
-        assert(torch.allclose(dbfpos.sum(), dbfpos_grad.sum()))
-        assert(torch.allclose(dbfpos, dbfpos_grad))
+        dbf_kernel = self.ao._backflow_kernel_derivative(ree)
+        assert(torch.allclose(dbf_kernel.sum(), dbf_kernel_auto.sum()))
+        assert(torch.allclose(dbf_kernel, dbf_kernel_auto))
 
-    def test_backflow_kernel_pos(self):
+    def test_second_derivative_backflow_kernel(self):
+        """Test the 2nd derivative of the kernel function
+           wrt the elec-elec distance."""
+
+        ree = self.ao.edist(self.pos)
+        bf_kernel = self.ao._backflow_kernel(ree)
+
+        d2bf_kernel_auto = hess_single_element(bf_kernel, ree)
+
+        d2bf_kernel = self.ao._backflow_kernel_second_derivative(ree)
+
+        assert(torch.allclose(d2bf_kernel.sum(), d2bf_kernel_auto.sum()))
+        assert(torch.allclose(d2bf_kernel, d2bf_kernel_auto))
+
+    def test_derivative_backflow_kernel_pos(self):
         """Test the derivative of the kenel function wrt the pos of the elecs.
-        Note that the derivative edist(pos,1) returns drr_ij = d/dx_i r_ij
+        Note that the derivative edist(pos,1) returns d r_ij = d/dx_i r_ij
         and that d/dx_j r_ij = d/d_xi r_ij = - d/dx_i r_ji
         i.e. edist(pos,1) returns half of the derivatives
 
@@ -108,8 +147,51 @@ class TestBFAOderivativesPyscf(unittest.TestCase):
                                          1).reshape(self.npts, -1)
         assert(torch.allclose(dbfpos, dbfpos_grad))
 
+    def test_second_derivative_backflow_kernel_pos(self):
+        """Test the derivative of the kenel function wrt the pos of the elecs.
+        Note that the derivative edist(pos,1) returns d r_ij = d/dx_i r_ij
+        and that d/dx_j r_ij = d/d_xi r_ij = - d/dx_i r_ji
+        i.e. edist(pos,1) returns half of the derivatives
+        Same thing for edist(pos,2)
+
+        so to obatin the same values than autograd we need to double d/dx_i r_ij
+        """
+
+        # compute the ee dist
+        ree = self.ao.edist(self.pos)
+
+        # compute the kernel values
+        bf_kernel = self.ao._backflow_kernel(ree)
+
+        # computes the derivative of the ee dist
+        di_ree = self.ao.edist(self.pos, 1)
+        dj_ree = di_ree
+
+        # computes the derivative of the ee dist
+        d2i_ree = self.ao.edist(self.pos, 2)
+        d2j_ree = d2i_ree
+
+        # compute the derivative of the kernel values
+        d2bf_kernel = 2*self.ao._backflow_kernel_second_derivative(
+            ree).unsqueeze(1) * di_ree * di_ree
+
+        d2bf_kernel += 2*self.ao._backflow_kernel_derivative(
+            ree).unsqueeze(1) * d2i_ree
+
+        # computes the the derivative of the kernal values with autograd
+        d2bf_kernel_auto = hess(bf_kernel, self.pos)
+
+        # checksum
+        assert(torch.allclose(d2bf_kernel.sum(), d2bf_kernel_auto.sum()))
+
+        # reshape and check individual elements
+        d2bf_kernel = d2bf_kernel.sum(-1).permute(0, 2,
+                                                  1).reshape(self.npts, -1)
+
+        assert(torch.allclose(d2bf_kernel, d2bf_kernel_auto))
+
     def test_backflow_derivative(self):
-        """Test the derivative pf the bf coordinate wrt the initial positions."""
+        """Test the derivative of the bf coordinate wrt the initial positions."""
 
         # compute backflow pos
         q = self.ao._backflow(self.pos)
@@ -130,6 +212,29 @@ class TestBFAOderivativesPyscf(unittest.TestCase):
         dq = dq.sum(-1).permute(0, 2,
                                 1).reshape(self.npts, self.mol.nelec*3)
         assert(torch.allclose(dq, dq_grad))
+
+    def test_backflow_second_derivative(self):
+        """Test the derivative of the bf coordinate wrt the initial positions."""
+
+        # compute backflow pos
+        q = self.ao._backflow(self.pos)
+
+        # compute der of the backflow pos wrt the
+        # original pos
+        d2q = self.ao._backflow_second_derivative(self.pos)
+
+        # compute der of the backflow pos wrt the
+        # original pos using autograd
+        d2q_auto = hess(q, self.pos)
+
+        # checksum
+        assert(torch.allclose(d2q.sum(), d2q_auto.sum()))
+
+        # permute and check elements
+        d2q = d2q.sum(-1).permute(0, 2,
+                                  1).reshape(self.npts, self.mol.nelec*3)
+
+        assert(torch.allclose(d2q, d2q_auto))
 
     def test_ao_gradian(self):
         """Test the calculation of the gradient of the at
@@ -176,8 +281,12 @@ class TestBFAOderivativesPyscf(unittest.TestCase):
 if __name__ == "__main__":
     t = TestBFAOderivativesPyscf()
     t.setUp()
-    t.test_backflow_kernel()
-    t.test_backflow_kernel_pos()
+    t.test_second_derivative_backflow_kernel()
+    t.test_second_derivative_backflow_kernel_pos()
+    t.test_backflow_second_derivative()
+
+    t.test_derivative_backflow_kernel()
+    t.test_derivative_backflow_kernel_pos()
     t.test_backflow_derivative()
     t.test_ao_gradian()
     t.test_ao_jacobian()