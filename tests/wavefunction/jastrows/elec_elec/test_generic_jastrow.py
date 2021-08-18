ab9a6cbd10bdb5e9254f45d3a946df6174440a6b
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-08-18 16:11:21 +0200

    fix jastrow test

--- tests/wavefunction/jastrows/elec_elec/test_generic_jastrow.py
+++ tests/wavefunction/jastrows/elec_elec/test_generic_jastrow.py
@@ -1,104 +1,35 @@
-import torch
-from torch.autograd import grad
-
 
 import unittest
-
 import numpy as np
 import torch
-from torch.autograd import Variable, grad
 
+
+from base_elec_elec_jastrow_test import BaseTestJastrow
+
+from types import SimpleNamespace
 from qmctorch.wavefunction.jastrows.elec_elec.jastrow_factor_electron_electron import JastrowFactorElectronElectron
 from qmctorch.wavefunction.jastrows.elec_elec.kernels.fully_connected_jastrow_kernel import FullyConnectedJastrowKernel
 
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 
-def hess(out, pos):
-    # compute the jacobian
-    z = Variable(torch.ones(out.shape))
-    jacob = grad(out, pos,
-                 grad_outputs=z,
-                 only_inputs=True,
-                 create_graph=True)[0]
-
-    # compute the diagonal element of the Hessian
-    z = Variable(torch.ones(jacob.shape[0]))
-    hess = torch.zeros(jacob.shape)
-
-    for idim in range(jacob.shape[1]):
-
-        tmp = grad(jacob[:, idim], pos,
-                   grad_outputs=z,
-                   only_inputs=True,
-                   create_graph=True)[0]
-
-        hess[:, idim] = tmp[:, idim]
-
-    return hess
-
-
-class TestGenericJastrow(unittest.TestCase):
+class TestGenericJastrow(BaseTestJastrow.ElecElecJastrowBaseTest):
 
     def setUp(self):
 
         torch.manual_seed(0)
         np.random.seed(0)
 
-        self.nup, self.ndown = 4, 4
-        self.nelec = self.nup + self.ndown
+        mol = SimpleNamespace(nup=4, ndown=4)
+        self.nelec = mol.nup + mol.ndown
+
         self.jastrow = JastrowFactorElectronElectron(
-            self.nup, self.ndown,
+            mol,
             FullyConnectedJastrowKernel)
         self.nbatch = 5
 
         self.pos = 1E-1 * torch.rand(self.nbatch, self.nelec * 3)
         self.pos.requires_grad = True
-
-    def test_jastrow(self):
-        """simply checks that the values are not crashing."""
-        val = self.jastrow(self.pos)
-
-    def test_grad_jastrow(self):
-        """Checks the values of the gradients."""
-        val = self.jastrow(self.pos)
-        dval = self.jastrow(self.pos, derivative=1, sum_grad=False)
-
-        dval_grad = grad(
-            val,
-            self.pos,
-            grad_outputs=torch.ones_like(val))[0]
-
-        dval_grad = dval_grad.reshape(
-            self.nbatch, self.nelec, 3).permute(0, 2, 1)
-
-        assert(torch.allclose(dval, dval_grad))
-
-    def test_jacobian_jastrow(self):
-        """Checks the values of the gradients."""
-        val = self.jastrow(self.pos)
-        dval = self.jastrow(self.pos, derivative=1)
-
-        dval_grad = grad(
-            val,
-            self.pos,
-            grad_outputs=torch.ones_like(val))[0]
-
-        dval_grad = dval_grad.reshape(
-            self.nbatch, self.nelec, 3).permute(0, 2, 1).sum(-2)
-
-        assert torch.allclose(dval, dval_grad)
-
-    def test_hess_jastrow(self):
-
-        val = self.jastrow(self.pos)
-        d2val = self.jastrow(self.pos, derivative=2)
-        d2val_grad = hess(val, self.pos)
-        # print(d2val)
-        # print(d2val_grad.reshape(
-        #     self.nbatch, self.nelec, 3).sum(2))
-        assert torch.allclose(d2val, d2val_grad.reshape(
-            self.nbatch, self.nelec, 3).sum(2))
 
 
 if __name__ == "__main__":
--- tests/wavefunction/jastrows/elec_elec/test_scaled_pade_jastrow_polynom.py
+++ tests/wavefunction/jastrows/elec_elec/test_scaled_pade_jastrow_polynom.py
@@ -1,8 +1,12 @@
-import unittest
 
+import unittest
 import numpy as np
 import torch
-from torch.autograd import Variable, grad, gradcheck
+
+
+from base_elec_elec_jastrow_test import BaseTestJastrow
+
+from types import SimpleNamespace
 
 from qmctorch.wavefunction.jastrows.elec_elec.jastrow_factor_electron_electron import JastrowFactorElectronElectron
 from qmctorch.wavefunction.jastrows.elec_elec.kernels.pade_jastrow_polynomial_kernel import PadeJastrowPolynomialKernel
@@ -10,89 +14,26 @@ from qmctorch.wavefunction.jastrows.elec_elec.kernels.pade_jastrow_polynomial_ke
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 
-def hess(out, pos):
-    # compute the jacobian
-    z = Variable(torch.ones(out.shape))
-    jacob = grad(out, pos,
-                 grad_outputs=z,
-                 only_inputs=True,
-                 create_graph=True)[0]
-
-    # compute the diagonal element of the Hessian
-    z = Variable(torch.ones(jacob.shape[0]))
-    hess = torch.zeros(jacob.shape)
-
-    for idim in range(jacob.shape[1]):
-
-        tmp = grad(jacob[:, idim], pos,
-                   grad_outputs=z,
-                   only_inputs=True,
-                   create_graph=True)[0]
-
-        hess[:, idim] = tmp[:, idim]
-
-    return hess
-
-
-class TestScaledPadeJastrowPolynom(unittest.TestCase):
+class TestScaledPadeJastrowPolynom(BaseTestJastrow.ElecElecJastrowBaseTest):
 
     def setUp(self):
 
         torch.manual_seed(0)
         np.random.seed(0)
 
-        self.nup, self.ndown = 4, 4
-        self.nelec = self.nup + self.ndown
+        mol = SimpleNamespace(nup=4, ndown=4)
+        self.nelec = mol.nup + mol.ndown
 
         self.jastrow = JastrowFactorElectronElectron(
-            self.nup, self.ndown, PadeJastrowPolynomialKernel,
+            mol, PadeJastrowPolynomialKernel,
             kernel_kwargs={'order': 5,
                            'weight_a': 0.1*torch.ones(5),
                            'weight_b': 0.1*torch.ones(5)},
             scale=True)
-
-        self.nbatch = 5
+        self.nbatch = 10
 
         self.pos = torch.rand(self.nbatch, self.nelec * 3)
         self.pos.requires_grad = True
-
-    def test_grad_distance(self):
-
-        r = self.jastrow.edist(self.pos)
-        dr = self.jastrow.edist(self.pos, derivative=1)
-        dr_grad = grad(
-            r,
-            self.pos,
-            grad_outputs=torch.ones_like(r))[0]
-        gradcheck(self.jastrow.edist, self.pos)
-
-        assert(torch.allclose(dr.sum(), dr_grad.sum(), atol=1E-5))
-
-    def test_grad_jastrow(self):
-
-        val = self.jastrow(self.pos)
-        dval = self.jastrow(self.pos, derivative=1)
-        dval_grad = grad(
-            val,
-            self.pos,
-            grad_outputs=torch.ones_like(val))[0]
-        gradcheck(self.jastrow, self.pos)
-
-        assert torch.allclose(dval, dval_grad.view(
-            self.nbatch, self.nelec, 3).sum(2))
-        assert(torch.allclose(dval.sum(), dval_grad.sum()))
-
-    def test_hess_jastrow(self):
-
-        val = self.jastrow(self.pos)
-        d2val_grad = hess(val, self.pos)
-        d2val_grad = d2val_grad.view(
-            self.nbatch, self.nelec, 3).sum(2)
-
-        d2val = self.jastrow(self.pos, derivative=2)
-
-        assert torch.allclose(d2val, d2val_grad)
-        assert(torch.allclose(d2val.sum(), d2val_grad.sum()))
 
 
 if __name__ == "__main__":
