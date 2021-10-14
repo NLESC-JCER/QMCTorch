6477409cb40ada895c8c2df1a745cd8e1d6376f8
Nicolas Renaud <nicolas.gm.renaud@gmail.com>
2021-10-14 09:46:02 +0200

    fix issue with reweighting sampling points

--- qmctorch/utils/torch_utils.py
+++ qmctorch/utils/torch_utils.py
@@ -236,7 +236,7 @@ class Loss(nn.Module):
             mask = self.get_clipping_mask(local_energies)
 
             # sampling_weight
-            weight = self.get_sampling_weights(deactivate_weight)
+            weight = self.get_sampling_weights(pos, deactivate_weight)
 
             # compute the loss
             loss = self.loss_fn((weight * local_energies)[mask])
@@ -273,7 +273,7 @@ class Loss(nn.Module):
 
         return mask
 
-    def get_sampling_weights(self, deactivate_weight):
+    def get_sampling_weights(self, pos, deactivate_weight):
         """Get the weight needed when resampling is not
             done at every step
         """
