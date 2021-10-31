3eaf0b5376eb454ab4f4bcb2f88f376122b003cd
matthijs <matthijs111@gmail.com>
2021-10-31 19:36:44 +0100

    sampling tests

--- example/sampling/sampling_test.sh
+++ example/sampling/sampling_test.sh
@@ -2,16 +2,13 @@
 #SBATCH -n 1
 #SBATCH -t 10:00:00
 
-export PYTHONPATH="$PYTHONPATH:/home/dewit/QMCTorch:/sw/arch/Debian10/EB_production/2020/software/ADF/2019.303-intelmpi/scripting"
-
-mkdir $TMPDIR/$USER
-mkdir $SCM_TMPDIR
+export PYTHONPATH="$PYTHONPATH:/home/dewit/QMCTorch"
 
 cd h2/ || exit
 python h2_m.py
 python h2_hmc.py
 
-#cd ../lih || exit
-#python lih_m.py
-#python lih_hmc.py
+cd ../lih || exit
+python lih_m.py
+python lih_hmc.py
 
