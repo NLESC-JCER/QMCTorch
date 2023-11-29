86f66d662a5c2edb8d99fc0cb905cdc2fc6dd98b
Nico <nicolas.gm.renaud@gmail.com>
2023-11-29 10:29:33 +0100

    pylint unused module in baseimport

--- h5x/baseimport.py
+++ h5x/baseimport.py
@@ -1,7 +1,7 @@
-from qmctorch.utils.plot_data import (
+from qmctorch.utils.plot_data import ( # pylint: disable=unused-import
     plot_energy, plot_data, plot_block, plot_walkers_traj)
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt # pylint: disable=unused-import
+import numpy as np # pylint: disable=unused-import
 
 print(r"  ____    __  ______________             _")
 print(r" / __ \  /  |/  / ___/_  __/__  ________/ /  ")
