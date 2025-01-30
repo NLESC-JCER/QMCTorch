1c029a8b12da8ff44c15c185fb1cf3317983e0b5
Nico <nicolas.gm.renaud@gmail.com>
2025-01-30 19:25:11 +0100

    fix version

--- qmctorch/__version__.py
+++ qmctorch/__version__.py
@@ -1,7 +1,1 @@
-import subprocess
-import os
-
 __version__ = "0.3.2"
-
-cwd = os.path.dirname(os.path.abspath(__file__))
-git_describe_tag = subprocess.check_output(["git", "describe", "--tags"], cwd=cwd).decode("utf-8").strip("\n")
