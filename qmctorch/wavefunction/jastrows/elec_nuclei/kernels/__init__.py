7318ce1b44928821de4f54eaf1aee2f9f9d377f6
Nico <nicolas.gm.renaud@gmail.com>
2025-05-09 13:44:46 +0200

    added quadratic en pade jastrow kernel

--- qmctorch/wavefunction/jastrows/elec_nuclei/kernels/__init__.py
+++ qmctorch/wavefunction/jastrows/elec_nuclei/kernels/__init__.py
@@ -1,9 +1,11 @@
 from .fully_connected_jastrow_kernel import FullyConnectedJastrowKernel
 from .jastrow_kernel_electron_nuclei_base import JastrowKernelElectronNucleiBase
 from .pade_jastrow_kernel import PadeJastrowKernel
+from .quadratic_pade_kernel import QuadraticPadeJastrowKernel
 
 __all__ = [
     "FullyConnectedJastrowKernel",
     "JastrowKernelElectronNucleiBase",
     "PadeJastrowKernel",
+    "QuadraticPadeJastrowKernel",
 ]
